1cd16599f7577984a1899c67a2e548a8f24ac14d
Bohdan Mukha <by.muha@gmail.com>
2023-02-13 16:57:54 +0200

    exercise 3.11 done

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static('build'));
 
 morgan.token('body', (req, res) => {
   const body = JSON.stringify(req.body);
